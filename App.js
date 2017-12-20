95a18b2bcf223a3a30e624153e846c9374c57468
AbhishekDa <abhishekda@hexaware.com>
2017-12-20 23:23:04 +0530

    stable build

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const agentBot = require('./lib/agentBot');
-// const readline = require('readline');
+const readline = require('readline');
 
 // const rl = readline.createInterface({
 //     input: process.stdin,
@@ -7,14 +7,14 @@ const agentBot = require('./lib/agentBot');
 // });
 
 // rl.question('Please enter account id: \n', (accountId) => {
-    //     rl.question('Please enter agent user name: \n', (userName) => {
-    //         rl.question('Please enter agent password: \n', (password) => {
-    //             const agent = new agentBot(accountId, userName, password);
-    //             rl.close();
-    //             agent.start();
-    //         });
-    //     });
-    // });
+//         rl.question('Please enter agent user name: \n', (userName) => {
+//             rl.question('Please enter agent password: \n', (password) => {
+//                 const agent = new agentBot(accountId, userName, password);
+//                 rl.close();
+//                 agent.start();
+//             });
+//         });
+//     });
 
 
 
